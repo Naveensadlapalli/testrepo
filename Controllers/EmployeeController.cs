d1755c224308725494ee115077e852a8b4abde2a
Naveen Kumar <naveensadlapalli@gmail.com>
2020-11-02 23:15:11 +0530

    Initial Push

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using RegistrationPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationPage.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        SystemTestEntities obj = new SystemTestEntities();
+        public ActionResult Index()
+        {
+            IEnumerable<Models.Registration> ie = obj.Registrations.ToList();
+            TempData["ie"] = ie;
+            TempData.Keep();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Models.Registration e1)
+        {
+            obj.Registrations.Add(e1);
+            obj.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        
+
+    }
+}
